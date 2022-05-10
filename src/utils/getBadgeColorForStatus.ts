79f0b2f310e3cf2e407057181e8c46e4ae604a31
Pankaj Patel <patel.pankaj@live.in>
2022-05-10 10:18:47 +0200

    cleanup

--- src/utils/getBadgeColorForStatus.ts
+++ src/utils/getBadgeColorForStatus.ts
@@ -1,21 +1,12 @@
 import { PalletteIdentifiers } from '../style/tokens';
-/*
-ready
-not_started
-paid
-in_progress
-default
-*/
+
 export const getBadgeColorForStatus = (status: Status): PalletteIdentifiers => {
   switch (status) {
-    case 'pending':
-      return 'warning';
     case 'paid':
       return 'success';
     case 'ready':
     case 'in_progress':
       return 'info';
-    case 'cancelled':
     case 'default':
       return 'error';
     case 'not_started':
